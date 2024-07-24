de035d24591bdb72fd2ffc98c23774c037c194e7
lukasmchedlishvili <mchedlishvililuka@gmail.com>
2024-07-24 18:44:01 +0400

    homework

---
+++ level17/level17.py
@@ -0,0 +1,18 @@
+for txt in range(31):
+    print('Goa is the best')
+
+for num in range(5,151):
+    print(num)
+
+for num in range(2, 51, 4):
+    print(num)
+
+for i in range(11):
+    print(i, 'GOA')
+
+for i in range(10):
+    print('even')
+    print('odd')
+
+
+
